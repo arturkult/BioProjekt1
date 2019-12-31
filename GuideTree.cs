bafcda3b4344847768e58d0140c505470ae0f9a6
Adam Wasilewski <adam.wasilewski.1995@gmail.com>
2019-12-30 22:15:47 -0800

    Implement printTree method

--- GuideTree.cs
+++ GuideTree.cs
@@ -3,12 +3,39 @@ namespace Projekt1
 {
     public class GuideTree
     {
+        public GuideTreeNode rootNode = null;
+
         public GuideTree()
         {
         }
 
         public void printTree()
         {
+            if (rootNode != null)
+            {
+                printSubtree(rootNode, "");
+            }
+
+        }
+
+        private void printSubtree(GuideTreeNode subtreeRoot, string indentation) {
+            if (subtreeRoot == null)
+            {
+                Console.WriteLine();
+            }
+            else if (subtreeRoot.leftChild == null || subtreeRoot.rightChild == null)
+            {
+                Console.WriteLine(indentation + subtreeRoot.name);
+            }
+            else
+            {
+                string additionalIndentation = "\t\t";
+
+                Console.Write(subtreeRoot.distanceToLeftChild + " -> ");
+                printSubtree(subtreeRoot.leftChild, indentation + additionalIndentation);
+                Console.Write(subtreeRoot.distanceToRightChild + " -> ");
+                printSubtree(subtreeRoot.rightChild, indentation + additionalIndentation);
+            }
 
         }
 
